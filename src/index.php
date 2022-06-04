3c1470902c8a0e57ea7924e13822b865b915e65c
Abdou KANDJI <kandjiabdou@github.com>
2022-06-04 17:01:28 +0200

    affichage des produits

--- src/index.php
+++ src/index.php
@@ -1,13 +1,4 @@
 <?php
-/*require_once 'common/CommonComponents.php';
-require_once 'common/AuthenticationService.php';
-require_once 'controller/CounterController.php';
-require_once 'database/DatabaseCompteurRepository.php';
-
-$counterController = new CounterController(new AuthenticationService(), new DatabaseCompteurRepository());
-$counterHtml = $counterController->viewAction();
-CommonComponents::render($counterHtml);
-*/
 
 require_once 'common/CommonComponents.php';
 require_once 'controller/ProductListController.php';
