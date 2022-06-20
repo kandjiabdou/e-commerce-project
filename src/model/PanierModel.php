0d9b15487a30b5a5d5e9748890517cf536289683
Abdou KANDJI <kandjiabdou@github.com>
2022-06-20 05:54:49 +0200

    readme

--- src/model/PanierModel.php
+++ src/model/PanierModel.php
@@ -15,27 +15,4 @@ class PanierModel{
     $produit->execute();
     return $produit->fetch();
   }
-
-  public function getNextPanierID(){
-    try {
-      $sql = 'SELECT max(panierID) FROM `panier`;';
-      $req = $this->database->prepare($sql);
-      $req->execute();
-      $tab = $req->fetch(PDO::FETCH_NUM);
-      return $tab ? $tab[0]+1 : 1;
-    } catch (PDOException $e) {
-      die('Echec getNextPanierID, erreur n°' . $e->getCode() . ':' . $e->getMessage());
-    }
-  }
-  public function insertPanier($panier){
-    try {
-      extract($panier);
-      $idPanier = $this->getNextPanierID();
-      $sql = "INSERT INTO `panier` (`panierID`, `userID`, `etatPanier`)VALUES ( :idPanier, :idUser, '1');";
-      $request = $this->database->prepare($sql);
-      $request->execute(['idPanier' => $idPanier,'idUser' => $idUsesr]);
-    } catch (PDOException $e) {
-      die('Echec insertPanier, erreur n°' . $e->getCode() . ':' . $e->getMessage());
-    }
-  }
 }
